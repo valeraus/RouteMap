3f59fe0aeab48c54447ce6fc8fdf1a585383941e
Valera <Valera@ASUSX556U>
2023-03-11 21:21:12 +0200

    Добавьте файлы проекта.

---
+++ RouteMap/JavaScript.js
@@ -0,0 +1,35 @@
+﻿let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 50.44952836682165, lng: 30.600016080711644 },
+        disableDefaultUI: true,
+        zoom: 14,
+    });
+
+//50.44952836682165, 30.600016080711644
+//50.48307483695035, 30.39718793209465
+    const marker = new google.maps.Marker({
+        position: {
+            lat: 50.44952836682165,
+            lng: 30.600016080711644
+        },
+        map: map,
+    });
+    const infowindow = new google.maps.InfoWindow({
+        content: "Відправна точка",
+    });
+    marker.addListener("click", () => {
+        infowindow.open(marker.get("map"), marker);
+    });
+
+
+    const home = { lat: 50.48307483695035, lng: 30.39718793209465 };
+    var button = document.getElementById("myButton");
+    button.addEventListener("click", () => {
+        map.setCenter(home);
+    });
+    return controlButton;
+
+}
+window.initMap = initMap;
---
+++ RouteMap/main.js
@@ -0,0 +1,17 @@
+﻿var app = new Vue({
+    el: "#app",
+    data: {
+        show: false,
+        message: "Tut"
+    },
+    methods: {
+        MenuShow() {
+            if (this.show == true) {
+                this.show = false
+            }
+            else {
+                this.show = true;
+            }
+        }
+    }
+})
